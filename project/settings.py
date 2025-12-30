ffb69e1d416a185c933b585836081d2d813dc91c
Amankwah Foster <multiwebgh@gmail.com>
2025-12-30 01:31:38 -0800

    Updated the static files in the settings

--- project/settings.py
+++ project/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "media"
+MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = [
     BASE_DIR / "mystaticfiles"
 ]
